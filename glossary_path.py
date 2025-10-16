4a676aff788cd48264d1adb392717a8cfa55263c
Daniel Guez <daniel.guez161101@gmail.com>
2025-10-16 17:14:38 +0200

    Create glossary_path.py

---
+++ glossary_path.py
@@ -0,0 +1,6 @@
+glossary = {
+    "BOAT": "PowerBI x AI/BOAT/glossaire_boat.xlsx",
+    "FDS" : [],
+    "Data Management" : "PowerBI x AI/Data Management/glossaire_data_management.xlsx",
+    "Autre" : ["BPCEVIE - IZD - Pilotage EVD - PROD", "BPCESA - NXHCC - ALM BOAT (Prod)", "BPCESA - NXHCC - GRL (Production)","BPCESA - NXDJO - Data Management - Prod","BPCESA - NXDJO - Data Management - Bench","BPCESA - NXDJO - Data Management - Dev"]
+}
